27c55fc46655bb92b10860664ca8f18ef02eb225
Jeethi Nair <jeethi@umich.edu>
2018-11-28 17:38:35 -0500

    Initial Commit

---
+++ ProjectFun/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ProjectFun
+//
+//  Created by Jeethi Nair on 11/28/18.
+//  Copyright © 2018 Jeethi Nair. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
